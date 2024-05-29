6f38a41bb9054a56b5339913b694be78b23b9ccf
Tom <mr.ivan.ivanovich777@mail.ru>
2024-05-29 19:09:23 +0300

    Update README.md

---
+++ pytho_trening/task_9_OOP.py
@@ -0,0 +1,20 @@
+class Button:
+
+
+    def __init__(self, text, link):
+        self.text = text
+        self.link = link
+
+
+#Создаем экземпляры класса
+home = Button ('Домой', '/home')
+catalog_msk = Button('Каталог', '/msk/catalog')
+
+# Получаем доступ к атрибутам
+print(home.text)
+print('Кнопка' + home.text + 'имеет ссылку' + home.link)
+
+print('\n')
+
+print(catalog_msk.text)
+print('Кнопка' + catalog_msk.text + 'имеет ссылку' + catalog_msk.text)

ed32d6c3e99ad94f04f5af0e26b6969ff6916f23
Tom <mr.ivan.ivanovich777@mail.ru>
2024-06-03 21:03:13 +0300

    Update README.md

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,10 +1,10 @@
-def test_passing():
-    assert  (1, 2, 3) == (1, 2, 3)
-
+# def test_passing():
+#     assert  (1, 2, 3) == (1, 2, 3)
 #
-# def test_fail():
-#     assert 'test' == 'testing'
-#
+# #
+# # def test_fail():
+# #     assert 'test' == 'testing'
+# #
 # функция для проверки на несоответствие
 def test_not():
     a = "test"
